89cebfcd03a8a04b31cadd5049b31390e787acde
shpark <dakmori95@gmail.com>
2022-07-29 09:33:36 +0900

    fgsgs

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,8 @@ namespace team_Project {
         private void button8_Click(object sender, EventArgs e)
         {
             //저장 누르면 모달로그 꺼내서 이미지 전체 저장 or 확대된 부분 저장
+            //asdf
+            //asdaf
         }
     }
 }
